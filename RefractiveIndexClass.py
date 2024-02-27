bd3abbdeac126b14142f4e32d1f328323b9655c1
Jack Morse <jack.morse2001@gmail.com>
2024-02-27 21:45:08 +0000

    Add gas index

--- RefractiveIndexClass.py
+++ RefractiveIndexClass.py
@@ -90,7 +90,7 @@ class RefractiveIndex:
     
         Returns
         -------
-        
+        The refractive index function as an array.
         '''
         import numpy as np
         if n_gas is None:
@@ -129,6 +129,75 @@ class RefractiveIndex:
             print(f"Argument 'part' in HCF should be one of: \nFor the real part of the index: {real_keyword_array}\nFor the imaginary (loss) part of the index: {imaginary_keyword_array}")
             return ValueError("Argument value for 'part' unrecognised.")
         return HCF_refractive_indices
+
+    # # # # # # # # # # # # # # # # # # # # # # # #
+    # Table for gas constants in Borzsonyi 2008  #
+    # # # # # # # # # # # # # # # # # # # # # # # #
+    
+    # Gas      | B_1 x 10^8 | C_1 x 10^6 | B_2 x 10^8 | C_2 x 10^3 | Std: Dev: x 10^8 | #
+    # -----------------------------------------------------------------------------------
+    # Air      |  14926.44  | 19.36      | 41807.57   | 7.434      | 16.23            |
+    # Nitrogen | 39209.95   | 1146.24    | 18806.48   | 13.476     | 5.46             |
+    # Helium   | 4977.77    | 28.54      | 1856.94    | 7.760      | 1.46             |
+    # Neon     | 9154.48    | 656.97     | 4018.63    | 5.728      | 0.41             |
+    # Argon    | 20332.29   | 206.12     | 34458.31   | 8.066      | 5.92             |
+    # Krypton  | 26102.88   | 2.01       | 56946.82   | 10.043     | 21.67            |
+    # Xenon    | 103701.61  | 12.75      | 31228.61   | 0.561      | 43.25            |
+    
+    def Gas(wavelengths, pressure, temperature, gas_name = None):
+        '''
+        Returns the refractive index of a gas for a given pressure and temperature. Based on Borzsonyi 2008 paper.
+        DOI: https://doi.org/10.1364/AO.47.004856
+        
+        Parameters
+        -------
+        wavelengths ([float]): An array of wavelengths in [nm]
+        pressure (float): Pressure of gas in mBar
+        temperature (float): Temperature of gas in Kelvin
+        gas_name (string): The name of the gas, one of: "Air", "Nitrogen", "Helium", "Neon", "Argon", "Krypton", "Xenon".
+    
+        Returns
+        -------
+        The refractive index function as an array of values.
+        '''
+        
+        # Gas coefficients from table
+        # array = [B_1*1e8, C_1*1e6, B_2*1e8, C_2*1e3]
+        air_array = np.array([14926.44, 19.36, 41807.57, 7.434])
+        nitrogen_array = np.array([39209.95, 1146.24, 18806.48, 13.476])
+        helium_array = np.array([4977.77, 28.54, 1856.94, 7.760])
+        neon_array = np.array([9154.48, 656.97, 4018.63, 5.728])
+        argon_array = np.array([20332.29, 206.12, 34458.31, 8.066])
+        krypton_array = np.array([26102.88, 2.01, 56946.82, 10.043])
+        xenon_array = np.array([103701.61, 12.75, 31228.61, 0.561])
+        
+        gases = {
+            "air": air_array,
+            "nitrogen": nitrogen_array,
+            "helium": helium_array,
+            "neon": neon_array,
+            "argon": argon_array,
+            "krypton": krypton_array,
+            "xenon": xenon_array
+        }
+
+        selected_gas = gases[str(gas_name).lower()]
+        if selected_gas is None:
+            print("Not a valid gas name. Gas must be one of: 'Air', 'Nitrogen', 'Helium', 'Neon', 'Argon', 'Krypton', 'Xenon'.")
+            return []
+        
+        B_1 = selected_gas[0] * 1e8
+        C_1 = selected_gas[1] * 1e6
+        B_2 = selected_gas[2] * 1e8
+        C_2 = selected_gas[3] * 1e3
+
+        p_0 = 1000      # mbars
+        T_0 = 273       # K
+        
+        n_squared_minus_1 = (pressure / p_0) * (T_0 / temperature) * ( (B_1 * wavelengths**2) / (wavelengths**2 - C_1) + (B_2) / (wavelengths**2 - C_2))
+        n = np.sqrt(n_squared_minus_1 + 1)
+        return n
+    
 
     def _deriv(f, wavelength, h=1e-6):
         return (1/(2  * h)) * (f(wavelength + h) - f(wavelength - h))
